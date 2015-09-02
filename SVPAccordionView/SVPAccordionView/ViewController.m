02c3dce042f201f8504cf22a471a0f41454f2f15
Svetoslav Popov <svetoslavpopov93@gmail.com>
2015-09-02 14:17:19 +0300

    Added SVPAccordionView to the repository

---
+++ SVPAccordionView/SVPAccordionView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SVPAccordionView
+//
+//  Created by Svetoslav Popov on 9/2/15.
+//  Copyright (c) 2015 Svetoslav Popov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ SVPAccordionView/SVPAccordionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SVPAccordionView
+//
+//  Created by Svetoslav Popov on 9/2/15.
+//  Copyright (c) 2015 Svetoslav Popov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
